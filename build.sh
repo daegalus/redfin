b63e1b8d4f1625cd1e6c0b8f67c374d65491b826
Yulian Kuncheff <yulian@kuncheff.com>
2024-12-16 12:01:51 +0100

    Add asus stuff

--- build.sh
+++ build.sh
@@ -4,20 +4,18 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+# Add Repos
+dnf5 config-manager addrepo --from-repofile=https://copr.fedorainfracloud.org/coprs/lukenukem/asus-kernel/repo/fedora-41/lukenukem-asus-kernel-fedora-41.repo
+dnf5 config-manager addrepo --from-repofile=https://copr.fedorainfracloud.org/coprs/lukenukem/asus-linux/repo/fedora-41/lukenukem-asus-linux-fedora-41.repo
 
-### Install packages
+# Install Kernel
+dnf5 -y remove kernel* && rm -r /root
+dnf5 dnf5 -y install --allowerasing kernel asusctl supergfxctl
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Install GPU Switcher
+curl -o gpu-switcher-supergfxctlchikobara.github.io.v9.shell-extension.zip https://extensions.gnome.org/extension-data/gpu-switcher-supergfxctlchikobara.github.io.v9.shell-extension.zip
+gnome-extensions install gpu-switcher-supergfxctlchikobara.github.io.v9.shell-extension.zip
+rm gpu-switcher-supergfxctlchikobara.github.io.v9.shell-extension.zip
 
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Refresh Drivers (primarily Nvidia) for new Kernel
+sudo akmods
